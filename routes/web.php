c751334c1aa707d06deda301e716663413f02a5a
kenichi <slow_life_1407@yahoo.co.jp>
2022-01-26 06:29:29 +0000

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'MessagesController@index');
+
+Route::resource('messages', 'MessagesController');
+
+// 以下の7つのルーティングは上一行(resourceメソッド)と同じ
+// Route::get('messages/{id}', 'MessagesController@show');
+// Route::post('messages', 'MessagesController@store');
+// Route::put('messages/{id}', 'MessagesController@update');
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// Route::get('messages', 'MessagesController@index')->name('messages.index');
+// Route::get('messages/create', 'MessagesController@create')->name('messages.create');
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
